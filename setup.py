1de80ab2eef462b91726288da67b02d0adba40a7
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-10-09 22:57:35 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # long_description=long_description,
+    name="DSC-Believer",
+    packages=setuptools.find_packages(),
+    install_requires = [
+        "datasets"
+    ]
+)
