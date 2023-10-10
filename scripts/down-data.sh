3254d9c3d6cd6a3a7281911b3a0a228230cfc8d1
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-10-10 14:33:31 +0700

    Feature: update down-data code

---
+++ scripts/down-data.sh
@@ -0,0 +1,8 @@
+# mkdir data
+
+cd data
+# wget -nc 
+gdown 1kCifEFEvL0GY77lY2XZZAm9esm04HtA9 
+# unzip data file
+# tar -xf train_val_images.zip
+python -m zipfile -e DSC-public.zip .
--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     name="DSC-Believer",
     packages=setuptools.find_packages(),
     install_requires = [
-        "datasets"
+        "datasets",
+        "gdown"
     ]
 )
