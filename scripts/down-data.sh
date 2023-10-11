08e9101152d7d0b1e0f9c69389a067e0cfd0ad5d
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-10-11 21:06:57 +0700

    Feature: Adding download preprocess data

--- scripts/down-data.sh
+++ scripts/down-data.sh
@@ -3,6 +3,8 @@ mkdir data
 cd data
 # wget -nc 
 gdown 1kCifEFEvL0GY77lY2XZZAm9esm04HtA9 
+gdown 1kVugTUWy-ZC3q0xklSjQnXjUHixmeMfX 
 # unzip data file
 # tar -xf train_val_images.zip
 python -m zipfile -e DSC-public.zip .
+python -m zipfile -e DSC-public-retrieval.zip .
