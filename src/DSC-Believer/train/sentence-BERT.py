e604d549cba904237385ccd5120929cf82536090
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-10-19 18:48:40 +0700

    Chore

--- src/DSC-Believer/train/sentence-BERT.py
+++ src/DSC-Believer/train/sentence-BERT.py
@@ -25,7 +25,6 @@ def main():
     path_root = os.getcwd()
     dataset = DatasetDict.load_from_disk(os.path.join(path_root, 'data/DSC-public-preprocess'))
     dataset['train'] = dataset['train'].filter(lambda example: example['verdict'] != 'NEI')
-    dataset['dataset_public_test'] = dataset['dataset_public_test'].filter(lambda example: example['verdict'] != 'NEI')
     dataset = dataset.map(
         create_triplet, 
         remove_columns = ["verdict", "id", "context", "claim", "evidence"],
