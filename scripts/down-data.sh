b2c08c27f0d6446826179578ab6ccb76f4355d19
Gnartiel <105764822+Gnartiel@users.noreply.github.com>
2023-10-26 18:18:51 +0700

    Update down-data.sh

--- scripts/down-data.sh
+++ scripts/down-data.sh
@@ -4,7 +4,9 @@ cd data
 # wget -nc 
 gdown 1kCifEFEvL0GY77lY2XZZAm9esm04HtA9 
 gdown 1kVugTUWy-ZC3q0xklSjQnXjUHixmeMfX 
+gdown 1qWzzOq6CYJv65L89zfJufLXfWT6q4T88
 # unzip data file
 # tar -xf train_val_images.zip
 python -m zipfile -e DSC-public.zip .
 python -m zipfile -e DSC-public-retrieval.zip .
+python -m zipfile -e DSC-public-preprocess.zip .
