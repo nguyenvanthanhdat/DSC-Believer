32042d15612832b0267ad396e38ec3eb5ad264a8
Gnartiel <105764822+Gnartiel@users.noreply.github.com>
2023-10-26 19:30:42 +0700

    Update test-data.sh

--- scripts/test-data.sh
+++ scripts/test-data.sh
@@ -6,6 +6,6 @@ pip install -e .
 cd ../DSC-Believer
 pip install -r requirements.txt
 sh scripts/down-data.sh
-python src/DSC-Believer/data/data_transform.py
+# python src/DSC-Believer/data/data_transform.py
 python src/DSC-Believer/train/sentence-BERT.py
 python src/DSC-Believer/test/test_sentence_bert.py
