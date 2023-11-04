620aa167f37eb5be002d85628f7e5664a7a043d2
Gnartiel <105764822+Gnartiel@users.noreply.github.com>
2023-11-04 13:42:36 +0700

    Update test_sentence_bert.py

--- src/DSC-Believer/test/test_sentence_bert.py
+++ src/DSC-Believer/test/test_sentence_bert.py
@@ -14,10 +14,10 @@ def mean_pooling(model_output, attention_mask):
 def main():
     path_root = os.getcwd()
     dataset = DatasetDict.load_from_disk(os.path.join(path_root, 'data/DSC-public-preprocess'))
-    # model_name = "Gnartiel/multi-sbert-v2"
+    model_name = "Gnartiel/multi-sbert-v2"
     # model_name = "Gnartiel/multi-sbert"
     # model_name = "Gnartiel/vietnamese-sbert"
-    model_name = "Gnartiel/vietnamese-sbert-v2"
+    # model_name = "Gnartiel/vietnamese-sbert-v2"
     tokenizer = AutoTokenizer.from_pretrain-v2ed(model_name)
     model = AutoModel.from_pretrained(model_name, use_auth_token='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').to('cuda')
 
