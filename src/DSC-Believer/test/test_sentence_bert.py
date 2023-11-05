3b9be2f81c3235c8fe0cab6b60b1a87000eb9b4c
HgThinker <102933694+HgThinker@users.noreply.github.com>
2023-11-05 17:49:22 +0700

    Update test_sentence_bert.py
    update model name

--- src/DSC-Believer/test/test_sentence_bert.py
+++ src/DSC-Believer/test/test_sentence_bert.py
@@ -17,7 +17,7 @@ def main():
     path_root = os.getcwd()
     dataset = DatasetDict.load_from_disk(os.path.join(path_root, 'data/DSC-public-preprocess'))
     print(dataset)
-    model_name = "HgThinker/multi-qa-mpnet-base-dot-v1"
+    model_name = "HgThinker/vietnamese-sbert"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name).to(device)
     dataset_test = dataset['test'].filter(lambda example: example['verdict'] != 'NEI')
