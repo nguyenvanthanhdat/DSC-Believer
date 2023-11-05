74d5ce78d9b779e68180ade839a13c72a39f5c80
Gnartiel <105764822+Gnartiel@users.noreply.github.com>
2023-11-05 20:30:38 +0700

    Update test_sentence_bert.py

--- src/DSC-Believer/test/test_sentence_bert.py
+++ src/DSC-Believer/test/test_sentence_bert.py
@@ -1,52 +1,126 @@
-from tqdm.notebook import tqdm
-import torch
-from transformers import AutoTokenizer, AutoModel
-import torch.nn.functional as F
 from datasets import DatasetDict
 import os
+from sentence_transformers import (
+    losses, 
+    SentenceTransformer,
+    SentencesDataset, util
+)    
+import matplotlib.pyplot as plt
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import DataLoader
+from sentence_transformers.readers import InputExample
+from tqdm.notebook import tqdm
+import torch
+from transformers.trainer_callback import TrainerCallback
+import gc
+from transformers import BertTokenizer, BertForSequenceClassification
+import numpy as np
+from transformers import AutoModel, AutoTokenizer, AutoModelForSequenceClassification, AutoModelForMultipleChoice, BertTokenizer, AdamW, Trainer, TrainingArguments, BertForSequenceClassification
+from sklearn import preprocessing
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn as nn
+import torch.optim as optim
+import numpy
+from torch import Tensor
+import json
 
-# Mean Pooling - Take attention mask into account for correct averaging
-def mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output[0].to('cuda')  # First element of model_output contains all token embeddings
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float().to('cuda')
-    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+def retrieval(example, model):
+  context_list = example['context']
+  claim = example['claim']
+  claim_embedding = model.encode(claim)
+  context_embedding = model.encode(context_list)
+  similar = context_list[np.argmax(util.dot_score(claim_embedding, context_embedding))]
+  example['retrieval'] = similar
+  return example
+def evaluate(test_loader, model, loss_fn):
+  model.eval()
+
+  eval_losses = []
+  with torch.no_grad():
+      for eval_batch in test_loader:
+          input_ids, attention_mask, labels = eval_batch
+          outputs = model(input_ids.to('cuda'), attention_mask=attention_mask.to('cuda'))
+          logits = outputs.logits.to('cuda')
+          loss = loss_fn(logits, labels.to('cuda'))
+          eval_losses.append(loss.item())
+
+  average_eval_loss = np.average(eval_losses)
+  print(f'Average Eval Loss: {average_eval_loss:.4f}')
+  return average_eval_loss
+    
+def preprocess_data(dataset, label_encoder, tokenizer):
+    inputs = tokenizer(
+        dataset['claim'],
+        dataset['retrieval'],
+        padding=True,
+        truncation=True,
+        return_tensors='pt'
+    )
+    labels = label_encoder.fit_transform(dataset['verdict'])
+    labels = torch.tensor(labels)
+    return inputs, labels
+
+class ClearMemory(TrainerCallback):
+    def on_epoch_end(self, epoch, logs=None):
+        gc.collect()
+        torch.cuda.empty_cache()
 
 def main():
     path_root = os.getcwd()
+    print('PREPARING DATA ===============================================================')
     dataset = DatasetDict.load_from_disk(os.path.join(path_root, 'data/DSC-public-preprocess'))
-    # model_name = "Gnartiel/multi-sbert-v2"
-    # model_name = "Gnartiel/multi-sbert"
-    # model_name = "Gnartiel/vietnamese-sbert"
-    model_name = "Gnartiel/vietnamese-sbert-v2"
+    model_name = "Gnartiel/vietnamese-sbert"
+    model = SentenceTransformer(model_name).to('cuda')
+    for split in dataset:
+        print(f"Preprocessing {split} dataset")
+        dataset[split] = dataset[split].map(retrieval, fn_kwargs={"model": model})
+        
+    print(dataset)
+    train_data = dataset['train']
+    val_data = dataset['validation']
+    test_data = dataset['test']
+
+    model_name = "Gnartiel/BERT-classify"
+    label_encoder = preprocessing.LabelEncoder()
+    model =  AutoModelForSequenceClassification.from_pretrained(model_name).to('cuda')
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModel.from_pretrained(model_name, use_auth_token='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').to('cuda')
+    optimizer = optim.AdamW(model.parameters(), lr=1e-5)
+    loss_fn = nn.CrossEntropyLoss()
 
-    dataset_test = dataset['validation'].filter(lambda example: example['verdict'] != 'NEI')
-    print(dataset_test)
 
-    n_samples_test = len(dataset_test)
-    predict_score = 0
-    for i in tqdm(range(n_samples_test)):
-        claim = dataset_test[i]['claim']
-        context = dataset_test[i]['context']
-        evidence = dataset_test[i]['evidence']
-
-        encoded_input = tokenizer([claim] + context, padding=True, truncation=True, return_tensors="pt").to('cuda')
-        with torch.no_grad():
-            output = model(**encoded_input)
-
-        sentence_embeddings = mean_pooling(output, encoded_input['attention_mask'])
-
-        claim_embedding = sentence_embeddings[0].unsqueeze(0)  # Extract claim embedding
-        context_embeddings = sentence_embeddings[1:]  # Extract context embeddings
-        cosine_scores = F.cosine_similarity(claim_embedding, context_embeddings)
-
-        max_score_index = torch.argmax(cosine_scores).item()
-        max_score_sentence = context[max_score_index]
-        if max_score_sentence == evidence:
-            predict_score += 1
+    test_inputs, test_labels = preprocess_data(test_data, label_encoder, tokenizer)
+     
+    bs = 50 
+    test_loader = DataLoader(test_dataset, batch_size=bs)
+        
+    best_model_state = torch.load('best_model.pth')
+    model.load_state_dict(best_model_state)
     
-    print(f"Performance of model with validation dataset: ", predict_score/n_samples_test*100, "%")
+    # Set the model to evaluation mode
+    model.eval()
+    predicted_labels = []
+    with torch.no_grad():
+        for data in test_loader:
+            inputs, labels = data
+            inputs = {k: v.to('cuda') for k, v in inputs.items()}
+            outputs = model(**inputs)
+            predicted_labels.extend(outputs.logits.argmax(dim=1).cpu().numpy().tolist())
+    predicted_labels = label_encoder.inverse_transform(predicted_labels)
+
+    for idx, row in enumerate(test_data):
+        row['verdict'] = predicted_labels[idx]
+        if row['verdict'] == 'NEI':
+            row['evidence'] = ""
+    new_data = {}
+    for i in test_data:
+        new_data[i['id']] = {
+            'verdict' : i['verdict'],
+            'evidence' : i['evidence']
+        }
+    with open('public_result.json','w) as f:
+              json.dump(new_data,f)
+    
 
 if __name__ == "__main__":
     main()
