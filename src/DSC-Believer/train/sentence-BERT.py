bedf03fa647b4e8f90074d1391aa2d5d091d81fd
HgThinker <nguyenhoanggia143@gmail.com>
2023-11-05 16:33:15 +0700

    test TripletEvaluator

--- src/DSC-Believer/train/sentence-BERT.py
+++ src/DSC-Believer/train/sentence-BERT.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from sentence_transformers.readers import InputExample
 from transformers.trainer_callback import TrainerCallback
+from sentence_transformers.evaluation import TripletEvaluator
 from tqdm.notebook import tqdm
 import gc
 import torch
@@ -40,16 +41,24 @@ def main():
         )
     print(dataset)
     dataset_train = dataset['train']
-    #n_examples = len(dataset_train)  
-    n_examples = 1
-    train_examples = []
+    max_iter = len(dataset_train)
+    n_examples = int(max_iter*0.8)  
+    n_examples = 1  
 
+    train_examples = []
     for i in range(n_examples):
         examples = dataset_train[i]['set']
         for example in examples:
             # print(example)
             train_examples.append(InputExample(texts=[example[0], example[1], example[2]]))
-    model = SentenceTransformer("sentence-transformers/multi-qa-mpnet-base-dot-v1")
+    
+    dev_examples = []
+    for i in range(n_examples, max_iter):
+        examples = dataset_train[i]['set']
+        for example in examples:
+            # print(example)
+            dev_examples.append(InputExample(texts=[example[0], example[1], example[2]]))
+    model = SentenceTransformer("HgThinker/multi-qa-mpnet-base-dot-v1")
     train_loss = losses.TripletLoss(model = model)
     train_dataset = SentencesDataset(train_examples, model)
     train_dataloader = DataLoader(train_dataset, batch_size=16)
@@ -60,10 +69,12 @@ def main():
     scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
     scheduler = "WarmupLinear"
     model_save_path = os.path.join(path_root, 'model/retrieval')
-
-    loss_values = model.fit(
+    evaluator = TripletEvaluator.from_input_examples(dev_examples,batch_size=16,name = 'dev')
+    model.fit(
         train_objectives=[(train_dataloader, train_loss)], 
         epochs=num_epochs,
+        evaluation_steps=1000,
+        evaluator=evaluator,
         scheduler=scheduler,
         optimizer_class=optim.AdamW,  
         optimizer_params={'lr': 1e-5, 'weight_decay': 0.01},
@@ -73,12 +84,12 @@ def main():
         save_best_model = True,
         show_progress_bar=True
     )
-    print("LOSS VALUES: ",loss_values)
-    plt.plot(loss_values)
-    plt.xlabel('Iterations')
-    plt.ylabel('Loss')
-    plt.title('Training Loss Plot')
-    plt.savefig('training_loss.png')
+
+    # plt.plot(loss_values)
+    # plt.xlabel('Iterations')
+    # plt.ylabel('Loss')
+    # plt.title('Training Loss Plot')
+    # plt.savefig('training_loss.png')
     # # model.push_to_hub('presencesw/DSC-Believer-SBERT')
     # model.save_to_hub(
     #     repo_name= "presencesw/DSC-Believer-SBERT_vTripletLoss",
