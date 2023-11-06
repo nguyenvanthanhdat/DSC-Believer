aa8f207c18bf938e9c79dfe534edc59a23d710c6
HgThinker <102933694+HgThinker@users.noreply.github.com>
2023-11-06 16:09:55 +0700

    Add files via upload

---
+++ scripts/pre-data.sh
@@ -0,0 +1,10 @@
+huggingface-cli login --token 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 
+cd ..
+git clone https://github.com/UKPLab/sentence-transformers.git
+cd sentence-transformers
+pip install -e .
+cd ../DSC-Believer
+pip install -r requirements.txt
+sh scripts/down-data.sh
+python src/DSC-Believer/data/data_transform.py
+# python src/DSC-Believer/test/test_sbert.py
