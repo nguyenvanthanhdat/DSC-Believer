6ae1d32ac13a858121f83aa5fcb65a81dbcbd390
HgThinker <102933694+HgThinker@users.noreply.github.com>
2023-11-06 17:14:21 +0700

    Update sentence-BERT.py
    Update model name

--- src/DSC-Believer/train/sentence-BERT.py
+++ src/DSC-Believer/train/sentence-BERT.py
@@ -59,7 +59,7 @@ def main():
         for example in examples:
             # print(example)
             dev_examples.append(InputExample(texts=[example[0], example[1], example[2]]))
-    model = SentenceTransformer("HgThinker/vietnamese-sbert")
+    model = SentenceTransformer("HgThinker/multi-qa-mpnet-base-dot-v1")
     train_loss = losses.TripletLoss(model = model)
     train_dataset = SentencesDataset(train_examples, model)
     train_dataloader = DataLoader(train_dataset, batch_size=16)
@@ -97,7 +97,7 @@ def main():
     #     exist_ok=True,
     # )
     model.save_to_hub(
-        repo_name= "HgThinker/vietnamese-sbert",
+        repo_name= "HgThinker/multi-qa-mpnet-base-dot-v1",
         exist_ok=True,
     )
     
